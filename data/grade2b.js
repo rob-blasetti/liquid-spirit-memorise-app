06c82aaf36fea3aeeae2bdf68ceefd5c6f84859b
Rob Blasetti <rblasett@gmail.com>
2025-07-12 21:50:35 +1000

    the rest of the book 3 branch 2 content

--- data/grade2b.js
+++ data/grade2b.js
@@ -173,50 +173,181 @@ export const quoteMap = {
     ],
   },
   '6-1': {
-    text: '',
+    prayer: 'O Lord! Unto Thee I repair for refuge, and toward all Thy signs I set my heart. O Lord! Whether traveling or at home, and in my occupation or in my work, I place my whole trust in Thee. Grant me then Thy sufficing help so as to make me independent of all things, O Thou Who art unsurpassed in Thy mercy! Bestow upon me my portion, O Lord, as Thou pleasest, and cause me to be satisfied with whatsoever Thou hast ordained for me. Thine is the absolute authority to command.',
+    text: 'Incline your hearts, O people of God, unto the counsels of your true, your incom- parable Friend.',
     references: [
         {
-
-        }
+            word: 'Incline',
+            examples: [
+                'Emmet noticed that all the flowers in the field leaned in the direction of the sun. The flowers were inclined towards the sun.',
+                'From the back of the classroom, Ayumi was not able to hear the teacher well, so she leaned forward and turned her head to listen to him. Ayumi inclined her ear towards the teacher in order to hear him better.'
+            ],
+        },
+        {
+            word: 'True',
+            examples: [
+                'The table in the kitchen was old and scratched, so Ludo decided to remove the layer of paint, revealing the beautiful wood underneath. When Ludo finished, the true color of the wood could be seen.',
+                'Odam always did what he promised. His words always matched his deeds. Odam was true to his word.'
+            ],
+        },
+        {
+            word: 'Incomparable',
+            examples: [
+                'The Himalaya Mountains are the tallest in the world. No other mountains compare with them in their grandeur. The Himalaya Mountains are incomparable in their majesty and grandeur.',
+                'The melody touched her heart like no other she had ever heard. She found the melody incomparable in its beauty.'
+            ],
+        },
     ],
   },
   '6-2': {
-    text: '',
-    references: [
+    text: 'O ye beloved of the Lord! Commit not that which defileth the limpid stream of love or destroyeth the sweet fragrance of friendship.',
+        references: [
         {
-
-        }
+            word: 'Commit',
+            examples: [
+                'When one is learning how to write, one will surely make a few errors. It is not unusual to commit errors when one is learning how to write.',
+                'Portia does her best to obey the laws of God. She does not want to commit any act that would displease Him.'
+            ],
+        },
+        {
+            word: 'Defile',
+            examples: [
+                'The garden had once been beautiful, but some of the people who visited it left garbage and litter when they departed. The visitors defiled the garden with the garbage they left.',
+                'The stream always ran clear and clean from the mountain. But a lot of chemicals and waste have been dumped into it recently. The stream has been defiled by waste being dumped into it.'
+            ],
+        },
+        {
+            word: 'Limpid',
+            examples: [
+                'The water in the pond was perfectly clear, and it was possible for Tibor to see many fish swimming along its bottom. Tibor could see to the very bottom of the limpid pond.',
+                'The water from the well is very clean and pure. Everyone felt refreshed to drink from the limpid waters of the well.'
+            ],
+        },
+        {
+            word: 'Destroy',
+            examples: [
+                'The young boy built a huge castle out of sand on the seashore, but when a wave came in and covered it, the castle was completely destroyed.',
+                'The farmers built a dam to stop the river from flooding onto their crops during the rainy season. Without the dam, their crops would be destroyed by the floods.'
+            ],
+        },
     ],
   },
   '6-3': {
-    text: '',
+    text: '. . . strive ye with all your might to create, through the power of the Word of God, genuine love, spiritual communion and durable bonds among individuals.',
     references: [
         {
-
+            word: 'Genuine',
+            examples: [
+                'The bag looked like it was made of leather, but it was really made of a plastic material. The bag was not made of genuine leather.',
+                'Some rings only look like they are gold, but the king’s ring is genuine. It is gold with a beautiful red stone.'
+            ],
+        },
+        {
+            word: 'Durable',
+            examples: [
+                'When choosing a fabric to make her children’s clothes, Mrs. Singh looked for one that would not tear easily. She chose a durable fabric that would last a long time.',
+                'The wheels on a tractor are big and thick so that they can go over rocks and dirt easily and still work fine. The wheels on a tractor are durable.'
+            ],
+        },
+        {
+            word: 'Bond',
+            examples: [
+                'A dog often feels an attachment to its master. There is a special bond between a dog and its master.',
+                'There is a strong bond between a mother whale and her young calf, and they stay close together for long distances in the ocean, with the mother feeding the calf and protecting it from enemies.'
+            ],
         }
     ],
   },
   '7-1': {
-    text: '',
+    prayer: 'O Thou kind Lord! These lovely children are the handiwork of the fingers of Thy might and the wondrous signs of Thy greatness. O God! Protect these children, graciously assist them to be educated and enable them to render service to the world of humanity. O God! These children are pearls, cause them to be nurtured within the shell of Thy loving-kindness. Thou art the Bountiful, the All-Loving.',
+    text: 'That one indeed is a man who, today, dedicateth himself to the service of the entire human race.',
     references: [
         {
-
+            word: 'Dedicate',
+            examples: [
+                'Dervi always pays close attention during art class and spends many hours preparing herself for each class. Dervi dedicates much of her time to the arts.',
+                'Hung Wei’s father has a vegetable garden. Every day Hung Wei helps his father weed and water the garden. Hung Wei is dedicated to helping his father.'
+            ],
+        },
+        {
+            word: 'Service',
+            examples: [
+                'Mumbi, Apiyo and Tocho cleaned the schoolyard after the storm had left it full of broken branches and leaves from the trees. They were happy to render this service to the school.',
+                'Sachiko walks four kilometers to give a children’s class. She is happy to walk this far each week because she wants nothing more than to be of service to the community.'
+            ],
+        },
+        {
+            word: 'Human Race',
+            examples: [
+                'At school we saw pictures of people from many different countries. All of these people belong to the human race.',
+                'The museum has musical instruments from all over the world. Looking at their many shapes and sizes, the children were reminded of both the diversity of the human race and of the love of people everywhere for music.'
+            ],
         }
     ],
   },
   '7-2': {
-    text: '',
+    text: '“Waste not your time in idleness and sloth. Occupy yourselves with that which profiteth yourselves and others.',
     references: [
         {
-
+            word: 'Idleness',
+            examples: [
+                'When Furio came home from school, he did his homework, then helped his mother and assisted his younger brothers and sisters with their studies. Furio did not rest for a single moment. He was never idle.',
+                'An old steam train ran through the town for many years but now it is no longer in use and sits idle by the side of the tracks.'
+            ],
+        },
+        {
+            word: 'Sloth',
+            examples: [
+                'We avoid laziness and sloth when we are active, using our minds and our energies at all times to serve others.',
+                'Iman does not give in to laziness and sloth, so he studies hard every day to become a mechanic. He wants to be a useful member of the community.'
+            ],
+        },
+        {
+            word: 'Profit',
+            examples: [
+                'Fei Li was very gifted at making crafts, which she had learned from her grandmother. Fei Li wanted to teach crafts to others so they could also develop the ability to make beautiful things. Fei Li wanted her talents to profit others.',
+                'The doctor looked at the old woman. He could see she was not well and told her that her health would improve if she spent more time in the sunshine. She would profit, he said, from taking a long walk outdoors every day.'
+            ],
         }
     ],
   },
   '7-3': {
-    text: '',
+    text: 'It is incumbent upon each one of you to engage in some occupation—such as a craft, a trade or the like. We have exalted your engagement in such work to the rank of worship of the one true God.',
     references: [
         {
-
+            word: 'Incumbent',
+            examples: [
+                'Parents must be responsible for making sure that their children receive an education. It is incumbent upon parents to see to their children’s education.',
+                'It is incumbent upon every person to pray each day.'
+            ],
+        },
+        {
+            word: 'Engage',
+            examples: [
+                'Jacob’s brothers all have different jobs. One is an electrician, one is a fisherman, one is a teacher, and one is a painter. Each of Jacob’s brothers is engaged in a different occupation.',
+                'Each day, Marwan goes to school, assists her brother with his studies, practices playing football, and helps with the household chores. Throughout the day, Marwan engages in many activities.'
+            ],
+        },
+        {
+            word: 'Occupation',
+            examples: [
+                'Ariadne spends most of her time looking after her family—caring for their home, tending to their well-being, and making sure they have what they need. Ariadne’s main occupation is the care of her family.',
+                'Penelope is an architect. She spends her days designing apartment buildings in which families live. Penelope is an architect by occupation.'
+            ],
+        },
+        {
+            word: 'Rank',
+            examples: [
+                'Firefighters must always follow the orders given to them by the head of the fire department. The head of the fire department holds the highest rank in the department.',
+                'After many years of wood-working, Alberto became very skilled in carpentry. He achieved the rank of Master Carpenter.'
+            ],
+        },
+        {
+            word: 'Worship',
+            examples: [
+                'Once a week, Miguel’s parents invite their neighbors to their home to pray for the well- being of their village and their families and to offer gratitude to God. They gather together to worship and praise God.',
+                'In every part of the world, one finds special places where people gather to give praise and thanks to God. People go to these places to worship God together.'
+            ],
         }
     ],
   }
