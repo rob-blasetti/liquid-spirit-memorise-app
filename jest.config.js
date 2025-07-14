68fc2015c397806f59db194ea3e7c4f52a5f8d01
Rob Blasetti <rblasett@gmail.com>
2025-07-14 20:28:52 +1000

    Add tests for WelcomeScreen navigation

--- jest.config.js
+++ jest.config.js
@@ -6,9 +6,10 @@ module.exports = {
     ...preset.transform,
     '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
   },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   transformIgnorePatterns: [
     // Transform these modules by Babel
     // Transform these external modules by Babel
-    'node_modules/(?!((jest-)?react-native|@react-native(-community)?|react-native-vector-icons|@fortawesome/react-native-fontawesome|react-native-image-picker|@flipxyz/react-native-boring-avatars|react-native-tab-view)/)',
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?|@react-navigation.*|react-native-tts|react-native-linear-gradient|react-native-vector-icons|@fortawesome/react-native-fontawesome|react-native-image-picker|@flipxyz/react-native-boring-avatars|react-native-tab-view)/)',
   ],
 };
