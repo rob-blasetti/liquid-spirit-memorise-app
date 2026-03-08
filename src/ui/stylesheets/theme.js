39c38c479bc02f04b50daa6c012dcfafe5a33f39
Rob Blasetti <rblasett@gmail.com>
2026-03-08 12:16:33 +1100

    Revert "Apply Nuri-inspired theme palette to liquid_spirit_memorise"
    This reverts commit e3b1d9f2fee1409d1a06dcdcb7d2eecc856dcef3.

--- src/ui/stylesheets/theme.js
+++ src/ui/stylesheets/theme.js
@@ -1,88 +1,79 @@
 const themeVariables = {
-  // Nuri-inspired dark theme palette
-  blueColor: '#0a488c',
-  greenColor: '#58db33',
-  redColor: '#e52f2f',
-  pinkColor: '#fc59f8',
-  greyColor: '#f3f3f3',
-  blackColor: '#000000',
-  whiteColor: '#ffffff',
+    blueColor: '#0a488c',
+    greenColor: '#58db33',
+    redColor: '#e52f2f',
+    pinkColor: '#fc59f8',
+    greyColor: '#f3f3f3',
+    blackColor: '#000000',
+    whiteColor: '#ffffff',
+    
+    // These will need manual color adjustments, as React Native doesn't support LESS functions like 'lighten' and 'darken'
+    lightGreyColor: '#f3f3f3', // Lighter grey (adjust manually)
+    darkGreyColor: '#e0e0e0',  // Darker grey (adjust manually)
+    
+    primaryColor: '#312783', // This references blueColor, so we replace it
+    primaryLightColor: '#3578a8', // Adjust this manually to be lighter blue
+    primaryLighterColor: '#5b9ac8', // Even lighter blue
+    // Gradient defaults for screens (e.g., background gradients)
+    gradientStart: '#312783',       // dark blue, same as primaryColor
+    gradientEnd: '#3578a8',         // lighter blue, same as primaryLightColor
+    
+    secondaryColor: '#58db33',
+    secondaryLightColor: '#72e457', // Adjust this manually to be lighter green
+    secondaryDarkColor: '#44b628',  // Darker green
+  
+    tertiaryColor: '#fc59f8',
+    tertiaryLightColor: '#ff85fc', // Lighter pink
+    tertiaryDarkColor: '#e046d1',  // Darker pink
+    
+    // Wood tones for puzzle pieces
+    woodLight: '#e4cda0',
+    woodMid: '#c8a26a',
+    woodDark: '#8b5a2b',
+    
+    neutralLight: '#f3f3f3',  // General light grey
+    neutralDark: '#e0e0e0',   // General dark grey
+    
+    buttonPrimaryBg: '#0a488c',
+    buttonPrimaryHoverBg: '#3578a8', // Hover for primary button
+    buttonSecondaryBg: '#58db33',
+    buttonSecondaryHoverBg: '#72e457', // Hover for secondary button
+    buttonDisabledBg: '#f3f3f3', // Disabled button background
+    buttonDisabledText: '#b3b3b3', // Lighter disabled button text
+    
+    borderColor: '#e0e0e0',  // Dark grey for borders
+    borderLightColor: '#f3f3f3',  // Light grey for light borders
+  
+    menuBgColor: '#0a488c',
+    menuTextColor: '#ffffff', // Assuming text inverse color is white
+    menuHoverBg: '#58db33',
+    menuHoverText: '#ffffff',
+    
+    formInputBg: '#ffffff',
+    formInputBorder: '#e0e0e0',
+    formInputFocusBorder: '#0a488c',
+    formErrorBorder: '#e52f2f',  // Assuming error color is red
+    formSuccessBorder: '#58db33', // Assuming success color is green
+    
+    alertSuccessBg: '#58db33',
+    alertErrorBg: '#e52f2f',
+    alertWarningBg: '#ffcc00', // Assuming a standard warning color
+    alertTextColor: '#ffffff',
 
-  lightGreyColor: '#f3f3f3',
-  darkGreyColor: '#e0e0e0',
-
-  // Nuri brand / app token set
-  primaryColor: '#312783',
-  primaryLightColor: '#3F78FF',
-  primaryLighterColor: '#C2D3FF',
-  // Gradient defaults for screens
-  gradientStart: '#312783',
-  gradientEnd: '#3F78FF',
-
-  secondaryColor: '#58db33',
-  secondaryLightColor: '#72e457',
-  secondaryDarkColor: '#44b628',
-
-  tertiaryColor: '#D96CFF',
-  tertiaryLightColor: '#E59CFF',
-  tertiaryDarkColor: '#AA34CC',
-
-  // Wood tones for puzzle pieces
-  woodLight: '#e4cda0',
-  woodMid: '#c8a26a',
-  woodDark: '#8b5a2b',
-
-  neutralLight: '#0a122a',
-  neutralDark: '#121c3b',
-
-  buttonPrimaryBg: '#312783',
-  buttonPrimaryHoverBg: '#43339C',
-  buttonSecondaryBg: '#58db33',
-  buttonSecondaryHoverBg: '#72e457',
-  buttonDisabledBg: '#1f2a52',
-  buttonDisabledText: '#b3b3b3',
-
-  borderColor: '#334155',
-  borderLightColor: '#4b5d89',
-
-  menuBgColor: '#312783',
-  menuTextColor: '#ffffff',
-  menuHoverBg: '#D96CFF',
-  menuHoverText: '#ffffff',
-
-  formInputBg: '#ffffff',
-  formInputBorder: '#4b5d89',
-  formInputFocusBorder: '#312783',
-  formErrorBorder: '#e52f2f',
-  formSuccessBorder: '#58db33',
-
-  alertSuccessBg: '#58db33',
-  alertErrorBg: '#e52f2f',
-  alertWarningBg: '#ffcc00',
-  alertTextColor: '#ffffff',
-
-  bottomNavBg: '#1f2a52',
-
-  fontFamily: "'Noto Sans', sans-serif",
-
-  borderRadiusSharp: 0,
-  borderRadiusPill: 20,
-  borderRadiusJumbo: 44,
-
-  // App-level dark theme accents (used via shared alias file)
-  darkBg: '#1c1453',
-  darkSurface: '#1f2a52',
-  darkCard: '#263569',
-  cardStroke: '#334155',
-  textOnDark: '#F6F7FF',
-  textOnSurface: 'rgba(246, 247, 255, 0.88)',
-  textMutedOnDark: 'rgba(246, 247, 255, 0.72)',
-
-  boxShadowCard: 'rgba(0, 0, 0, 0.16) 0px 10px 36px 0px, rgba(0, 0, 0, 0.06) 0px 0px 0px 1px',
-  boxShadowInfo: '0px 0px 12px -9px',
-
-  pad: 10,
-  margin: 10,
-};
-
-export default themeVariables;
+    bottomNavBg: '#4B2770',
+    
+    fontFamily: "'Noto Sans', sans-serif",
+    
+    borderRadiusSharp: 0,
+    borderRadiusPill: 20,
+    borderRadiusJumbo: 44,
+    
+    boxShadowCard: 'rgba(0, 0, 0, 0.16) 0px 10px 36px 0px, rgba(0, 0, 0, 0.06) 0px 0px 0px 1px',
+    boxShadowInfo: '0px 0px 12px -9px',
+    
+    pad: 10,
+    margin: 10
+  };
+  
+  export default themeVariables;
+  
